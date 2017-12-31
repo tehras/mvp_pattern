9b38e5c070ff283236b2be4db1250302eed489dd
Taras Koshkin <tkoshkin@gmail.com>
2017-12-30 23:17:52 -0500

    Remove provides context

--- mvppattern/src/main/java/com/github/tehras/mvppattern/base/di/FragmentModule.kt
+++ mvppattern/src/main/java/com/github/tehras/mvppattern/base/di/FragmentModule.kt
@@ -1,6 +1,5 @@
 package com.github.tehras.mvppattern.base.di
 
-import android.content.Context
 import android.support.v4.app.Fragment
 import com.github.tehras.mvppattern.FragmentScope
 import dagger.Module
@@ -12,8 +11,4 @@ abstract class FragmentModule(private val fragment: Fragment) {
     @Provides
     @FragmentScope
     fun provideFragment(): Fragment = fragment
-
-    @Provides
-    @FragmentScope
-    fun provideFragmentContext(): Context? = fragment.activity
 }
