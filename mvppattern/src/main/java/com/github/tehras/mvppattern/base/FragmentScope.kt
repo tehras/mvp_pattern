4f38fa649016dfe482f8f375f70a60d2c1818b15
Taras Koshkin <tkoshkin@gmail.com>
2017-12-30 22:49:29 -0500

    removed redundant FragmentScope

--- mvppattern/src/main/java/com/github/tehras/mvppattern/base/FragmentScope.kt
+++
@@ -1,6 +0,0 @@
-package com.sash.cashregister.ui
-
-import javax.inject.Scope
-
-@Scope
-annotation class FragmentScope
--- mvppattern/src/main/java/com/github/tehras/mvppattern/base/di/FragmentModule.kt
+++ mvppattern/src/main/java/com/github/tehras/mvppattern/base/di/FragmentModule.kt
@@ -2,7 +2,7 @@ package com.github.tehras.mvppattern.base.di
 
 import android.content.Context
 import android.support.v4.app.Fragment
-import com.sash.cashregister.ui.FragmentScope
+import com.github.tehras.mvppattern.FragmentScope
 import dagger.Module
 import dagger.Provides
 
